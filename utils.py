0c283b3b057d0d3a4730119e9bda797013e33a2e
HileZ <56227515+HileZ360@users.noreply.github.com>
2024-06-01 20:04:32 +0300

    Exception Handling.

--- utils.py
+++ utils.py
@@ -1,16 +1,26 @@
 import json
 import re
 
-def load_json_file(filename, default):
+def load_json_file(filename: str, default: dict) -> dict:
     try:
         with open(filename, "r") as file:
             return json.load(file)
     except FileNotFoundError:
+        print(f"File not found: {filename}. Using default data.")
+        return default
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON file: {filename}. Using default data.")
+        return default
+    except Exception as e:
+        print(f"Error loading JSON file: {str(e)}")
         return default
 
-def save_json_file(filename, data):
-    with open(filename, "w") as file:
-        json.dump(data, file, indent=4)
+def save_json_file(filename: str, data: dict):
+    try:
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=4)
+    except Exception as e:
+        print(f"Error saving JSON file: {str(e)}")
 
-def validate_date(date_string):
+def validate_date(date_string: str) -> bool:
     return re.match(r"^\d{4}-\d{2}-\d{2}$", date_string) is not None
