d61a7a3cc251b45b7fe442bb345d67d422eba5f9
HileZ <56227515+HileZ360@users.noreply.github.com>
2024-06-01 20:02:36 +0300

    Added a window in case of an error.

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main(page: ft.Page):
     page.title = "CryptGuardian"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
-    create_gui(page)
+    page.full_screen = True  # Set full screen mode
+    try:
+        create_gui(page)
+    except Exception as e:
+        print(f"Error creating GUI: {e}")
 
 if __name__ == "__main__":
     ft.app(target=main)
