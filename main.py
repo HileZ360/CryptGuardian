6d6aa21ae05c2a56b5fe5c90e04e9d9b74b42b05
HileZ <56227515+HileZ360@users.noreply.github.com>
2024-07-11 22:52:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+# main.py
 import flet as ft
 from gui import create_gui
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main(page: ft.Page):
     page.title = "CryptGuardian"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
-    page.full_screen = True  # Set full screen mode
+    page.full_screen = True
     try:
         create_gui(page)
     except Exception as e:
-        print(f"Error creating GUI: {e}")
+        logging.error(f"Error creating GUI: {e}")
 
 if __name__ == "__main__":
     ft.app(target=main)
